df8fd02f5d52e99bd8c89c717ae71e6b8fae3638
MrAbbas <14amirabas@gmail.com>
2025-04-22 12:18:25 +0330

    install react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <main>
+        <Routes>
+          <Route path={'/'} element={"Hi"} />
+        </Routes>
+      </main>
     </div>
   );
 }
